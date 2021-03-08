38ef206894aa6b41d8f62c0b486b628d668a9e2f
oleksiikachmar <oleksii.kachmar@118118money.com>
2021-03-08 20:26:25 +0200

    update

--- AsynchronouseProgramming/AsynchronouseProgramming/TaskForStudents.cs
+++ AsynchronouseProgramming/AsynchronouseProgramming/TaskForStudents.cs
@@ -5,12 +5,10 @@ using System.Threading;
 
 namespace AsynchronouseProgramming
 {
-
     /// The assignment is to create code that would output 123456789... to console
     /// Two threads should start in parallel calculating each part of the string 1234 and 56789...
     /// The order is important how threads append strings to 'result' variable.
-    /// Tips: Use  new ManualResetEvent(false);    manualResetEvent.Set();  manualResetEvent.WaitOne();
-    /// Use thread Join methods
+    /// Tips: Use  new ManualResetEvent(false);    manualResetEvent.Set();  manualResetEvent.WaitOne();. Use thread Join methods
     class TaskForStudents
     {
         public static string result = "";
@@ -20,8 +18,6 @@ namespace AsynchronouseProgramming
             SumCalculator sumCalculator = new SumCalculator();
             var first = new Thread(() => { sumCalculator.Calculate(1, 495, null); });
             var second = new Thread(() => { sumCalculator.Calculate(495, 500, null); });
-
-            //TODO Add logic here
 
             Console.WriteLine(result);
             Console.ReadLine();
