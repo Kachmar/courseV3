b3f3cc805fd5af191bb8800e32b1c73e839dda20
oleksiikachmar <oleksii.kachmar@118118money.com>
2021-03-23 09:55:30 +0200

    web api task

---
+++ WebApi/Demo/ASP.NET_CORE_WEB.API/ASP.NET_CORE_WEB.API/Controllers/CustomWithoutBaseClassController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_CORE_WEB.API.Controllers
+{
+    using Microsoft.AspNetCore.Routing;
+    [Route("Custom")]
+    public class CustomWithoutBaseClassController
+    {
+        //Somehow when navigating to /prefix/CustomWithoutBaseClass/Abracadabra route, it calls Abracadabra
+        public string Abracadabra()
+        {
+            return "Value from Abracadabra";
+        }
+
+
+        //Having the class without any attributes, it is enough to have route attr on method
+        // to serve web request
+        [Route("")]
+        [Route("index")]
+        [Route("MyAction")]
+        public string WithRoute()
+        {
+
+            return "With route";
+        }
+
+        // This is an example of the need ActionResult, because we want to return either string result
+        // or http status code
+        // multiple return value
+        // api/WithActionResult?returnError=true
+        [Route("[action]")]
+        public ActionResult<string> WithActionResult([FromQuery] bool returnError)
+        {
+            if (returnError)
+            {
+                return new BadRequestResult();
+            }
+
+            return "With route";
+        }
+
+    }
+}
---
+++ WebApi/Demo/ASP.NET_CORE_WEB.API/ASP.NET_CORE_WEB.API/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_CORE_WEB.API.Controllers
+{
+    using Microsoft.Extensions.Options;
+
+    public class StudentsController : ControllerBase
+    {
+        private readonly IOptions<ConfigurationContainer> options;
+
+        public StudentsController(IOptions<ConfigurationContainer> options)
+        {
+            this.options = options;
+        }
+        //Somehow when navigating to /api/students rout, it calls Abracadabra
+        public async Task<string> Abracadabra()
+        {
+            var routeDAta = RouteData;
+            return "Tet";
+        }
+    }
+}
